fa9fbd505d4290fdaf0ffba7d1f0b1440f097e16
Sentissi Mouad <m.sentissi@gmail.com>
2023-01-30 11:35:08 +0100

    -

--- src/main/java/org/mycompany/Application.java
+++ src/main/java/org/mycompany/Application.java
@@ -3,7 +3,13 @@ package org.mycompany;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+
+import javax.xml.namespace.QName;
+//import org.apache.camel.dataformat.soap.name.ServiceInterfaceStrategy;
+
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.xml.Namespaces;
+import org.apache.camel.model.dataformat.SoapJaxbDataFormat;
 
 @SpringBootApplication
 @ImportResource({"classpath:spring/camel-context.xml"})
@@ -14,10 +20,19 @@ public class Application extends RouteBuilder {
 
     @Override
     public void configure() {
-        // populate the message queue with some messages
+        
+        //SoapJaxbDataFormat soapWSReceiptTransferRequest = new SoapJaxbDataFormat(WSReceiptTransferRequest.class.getPackage().getName(), new QNameStrategy(new QName("http://ws.example.com","exampleDataResponse")));
+        //SoapJaxbDataFormat soapWSReceiptTransferRequest = new SoapJaxbDataFormat("com.example.customerservice", new ServiceInterfaceStrategy(WSReceiptTransferRequest.class));
+
+        //Namespaces ns0 = new Namespaces("", "urn:Ariba:Buyer:vsap");
         from("netty4-http:http:0.0.0.0:8086")
-        .process(new MyProcessor());
-       // .to("xquery:Receipt_Transfer_Header_Cleaned.Xquery");
+        //.streamCaching()
+        //from("jetty:http://0.0.0.0:8123/foo")
+        //.process(new MyProcessor());
+        .to("xquery:Receipt_Transfer_Header_Cleaned.Xquery?dataFormat=CXF_MESSAGE&runLoggingLevel=TRACE");
+        //.marshal(soapWSReceiptTransferRequest)
+        //.transform().xquery("resource:classpath:Receipt_Transfer_Header_Cleaned.Xquery", ns0);
+        //.transform().xquery("resource:classpath:Receipt_Transfer_Header_Cleaned.Xquery");
     }
 
 }
--- src/main/java/org/mycompany/MyProcessor.java
+++ src/main/java/org/mycompany/MyProcessor.java
@@ -1,14 +1,80 @@
 package org.mycompany;
 
 import org.apache.camel.Exchange;
+
 import org.apache.camel.Processor;
+import org.apache.camel.component.cxf.CxfPayload;
+import org.apache.camel.util.CastUtils;
+import org.apache.cxf.binding.soap.SoapHeader;
+import org.apache.cxf.binding.soap.SoapMessage;
+import org.apache.cxf.headers.Header;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Element;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.soap.*;
 
 public class MyProcessor implements Processor {
 
+   /*  public void processData(
+            @Header("org.apache.cxf.headers.Header.list") List<SoapHeader> soapHeaders,
+            Exchange exchange) {       
+ 
+        // Print SOAP Headers
+        for (SoapHeader soapHeader : soapHeaders) {
+            System.out.println(soapHeader.getName().getLocalPart() + "  : "
+                             + ((Element)soapHeader.getObject()).getTextContent());
+        }
+ 
+    }*/
+
     public void process(Exchange exchange) throws Exception {
-        String payload = exchange.getIn().getBody(String.class);
+
+        // Get some HTTP Header with key key1 :
+            /*String val1 = exchange.getIn().getHeader("key1", String.class);
+            System.out.println(val1);*/
+
+        // Get the HTTP Body as String :
+            /*String body = exchange.getIn().getBody(String.class);
+            System.out.println(body);*/
+        
+
+           /*CxfPayload body = exchange.getIn().getBody(CxfPayload.class);
+            System.out.println(body);*/
+
+            /*Logger LOGGER = LoggerFactory.getLogger(MyProcessor.class);
+            
+            Object payload = exchange.getIn().getBody();
+            LOGGER.info("Got message: " + payload);*/
+            //SoapMessage soapBody = (SoapMessage) payload.getBody();
+            //System.out.println(soapBody);
+
+          
+            /*SoapMessage soapMessage = (SoapMessage)exchange.getIn().getHeader("CamelCxfMessage");
+            List<org.apache.cxf.headers.Header> aaa = ((SoapMessage) soapMessage.getExchange()).getHeaders();
+            for (Object header : aaa) {
+                SoapHeader soapHeader = (SoapHeader) header;
+                org.w3c.dom.Element element = (Element) soapHeader.getObject();
+                Element transactionID = (Element) element.getElementsByTagName("ns1:service").item(0);
+
+                System.out.println(transactionID.getTextContent());
+            }*/
+
+        /*SOAPPart part = request.getSOAPPart();
+        SOAPEnvelope env = part.getEnvelope();
+        SOAPHeader header = env.getHeader();
+        if (header == null) {
+            // Throw an exception
+        }*/
+
+        //String payload = exchange.getIn().getBody(String.class);
+       
+       /* String payload = exchange.getIn().getBody(String.class);
         System.out.println(payload);
-        exchange.getIn().setBody(payload);
+        exchange.getIn().setBody(payload); */
     }
 
 }

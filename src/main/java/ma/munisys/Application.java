42d230e4ef956f117c35d4f205d333c8d0c4e57a
Sentissi Mouad <m.sentissi@gmail.com>
2023-02-06 14:42:19 +0100

    camel loggin in.headers

--- src/main/java/ma/munisys/Application.java
+++ src/main/java/ma/munisys/Application.java
@@ -42,7 +42,8 @@ public class Application extends RouteBuilder {
                 + "${headers." + Exchange.HTTP_HOST + "}:"
                 + "${headers." + Exchange.HTTP_PORT + "}"
                 + "${headers." + Exchange.HTTP_PATH + "}")
-            .log(LoggingLevel.INFO, "Backendend response : \n${header}\n${body}")
+            .log(LoggingLevel.INFO, "Backend response in.headers: \n${in.headers}")
+            .log(LoggingLevel.INFO, "Backend response body: \n${body}")
             .to("xquery:Receipt_Transfer_Transformation_Response.Xquery");
         
                 from("direct:muis_trans_req_header")
